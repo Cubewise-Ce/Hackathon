8fa06dc4cd19aac2a70c65643245084876405bac
CLM <clemaitre@cubewise.com>
2018-03-08 18:31:54 +0800

    Directive working

--- js/controllers/my-js.js
+++ js/controllers/my-js.js
@@ -1,26 +1,20 @@
+(function(){
+  var app = angular.module('app');
+  app.directive('dynamicColspan', function(){
+    return {
+      restrict: 'A',
+      scope: {
+        year: '@',
+        currentYear: '@',
+        listsMonths: '=',
+        listsQuarters: '='
+      },
+      link:function($scope, $elements, $attributes){
+        var myList = year == currentYear ? listsQuarters : listsMonths ;
+        var colspanCOunt = myList.length + 2 ;
+        $elements.attr('colspan', colspanCOunt);
+      }
+    };
+  });
+})();
 
-
-// app.directive('dynamicColspan', ['$compile', dynamicColspanDirective]);
-
-// function dynamicColspanDirective($compile) {
-// return {
-// // restrict: 'A',
-// template: '',
-// link: function(scope, elem, attr) {
-//         function setColspan() {
-//           var colsToSpan = scope.$eval(attr.dynamicColspan);
-//           if(angular.isObject(colsToSpan)) {
-//             angular.forEach(colsToSpan, function(condition, key) {
-//               if(condition && isFinite(key)) {
-//                 colsToSpan = key;
-//               }
-//             }); 
-//           }
-          
-//           elem.attr('colspan', parseInt(colsToSpan, 10));
-//         }
-//         setColspan();
-//         scope.$watch(attr.dynamicColspan, setColspan);
-//       }
-// };
-// }
